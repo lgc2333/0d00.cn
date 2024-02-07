e5381df1e12b51db90fb14bf6da5165aa2d1574e
student_2333 <lgc2333@126.com>
2024-02-07 16:02:42 +0000

    f

--- js/index.js
+++ js/index.js
@@ -1,33 +1,32 @@
 window.onload = () => {
   window.player = new APlayer({
-    container: document.getElementById('aplayer'),
+    container: document.getElementById("aplayer"),
     // fixed: true,
     mini: true,
-    loop: 'none',
+    loop: "none",
     audio: [
       {
-        name: 'Voice',
-        artist: '綾地寧々',
-        url: 'assets/see-mine-0721.ogg',
+        name: "Voice",
+        artist: "綾地寧々",
+        url: "assets/see-mine-0721.ogg",
       },
       {
-        name: '恋せよ乙女！',
-        artist: '米倉千尋',
-        url: 'http://music.163.com/song/media/outer/url?id=34324540.mp3',
+        name: "恋せよ乙女！",
+        artist: "米倉千尋",
+        url: "http://music.163.com/song/media/outer/url?id=34324540.mp3",
       },
     ],
   });
 
   /** @type {HTMLImageElement} */
-  const bg = document.getElementsByClassName('bg')[0];
+  const bg = document.getElementById("bg");
   bg.style.opacity = 1;
-  bg.addEventListener('click', () => {
-    window.location.href = 'https://www.bilibili.com/video/BV1Lz4y1v7xa';
+  bg.addEventListener("click", () => {
+    window.location.href = "https://www.bilibili.com/video/BV1Lz4y1v7xa";
   });
 
-  alert('本网站有声，请注意音量~');
   setTimeout(() => {
     window.player.play();
-    document.title = '私のオナニーを見てください！';
+    document.title = "私のオナニーを見てください！";
   }, 1000);
 };
